435af0e4134d19419e190a8ca061cc3fcd6fc358
Glorison <lai.glorison@gmail.com>
2021-10-30 17:03:33 +1100

    Edited config

--- app/next.config.js
+++ app/next.config.js
@@ -1,28 +1,27 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 const defaultExports = {
+  reactStrictMode: true,
   swcMinify: true,
-}
+};
 
 const devExports = {
   env: {
     authDomain: process.env.AUTHDOMAIN,
     authClientId: process.env.AUTHAPIKEY,
-  }
-}
+  },
+};
 
-const prodExports = {
-
-}
+const prodExports = {};
 
 module.exports = (phase, { defaultConfig }) => {
-  return (phase === PHASE_DEVELOPMENT_SERVER) ?
-    {
+  return phase === PHASE_DEVELOPMENT_SERVER
+    ? {
       ...defaultExports,
-      ...devExports
-    } :
-    {
-      ...defaultExports,
-      ...prodExports
+      ...devExports,
     }
+    : {
+      ...defaultExports,
+      ...prodExports,
+    };
 };
